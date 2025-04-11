3b6ad24f5b81615f9400befe31c27ef13902d34f
minseunghwan <minccc42710@gmail.com>
2025-04-11 17:16:59 +0900

    2-7

---
+++ src/chapter2/interitance/Child.java
@@ -0,0 +1,5 @@
+package chapter2.interitance;
+
+public class Child extends Parent{
+
+}
---
+++ src/chapter2/interitance/Parent.java
@@ -0,0 +1,10 @@
+package chapter2.interitance;
+
+public class Parent {
+    public String familyName = "스파르탄";
+    public int honor = 10;
+
+    public void introduceFamily(){
+        System.out.println("우리 " + this.familyName + "가문은 .... ");
+    }
+}
