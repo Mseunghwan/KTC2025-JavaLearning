a8c185b3e7d1106f66a13b1a4eb544de1361bb80
minseunghwan <minccc42710@gmail.com>
2025-04-16 15:23:07 +0900

    3-2

--- src/chapter3/optional/Camp.java
+++ src/chapter3/optional/Camp.java
@@ -1,4 +1,18 @@
 package chapter3.optional;
 
+import java.util.Optional;
+
 public class Camp {
+    private Student student;
+
+    public Optional<Student> getStudent() {
+        // 이 코드 실행시점에 student가 널이면, optional이 널을 감싸서 리턴,
+        // null이 아니면 데이터를 감싸서 optional 객체가 만들어져서 리턴
+        // 그냥 상자 하나를 씌워서 보내는거
+        return Optional.ofNullable(student);
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
 }
--- src/chapter3/optional/Main.java
+++ src/chapter3/optional/Main.java
@@ -1,4 +1,44 @@
 package chapter3.optional;
 
+import java.util.Optional;
+
 public class Main {
+    public static void main(String[] args) {
+        Camp camp = new Camp();
+
+        // Optional 객체 활용
+        Optional<Student> studentOptional = camp.getStudent();
+        boolean flag = studentOptional.isPresent();
+        if(flag) {
+            Student student = studentOptional.get();
+            String studentName = student.getName();
+            System.out.println(studentName);
+
+        } else {
+            System.out.println("학생 데이터가 없습니다");
+        }
+
+
+
+        // 1. student = null
+//        Student student = camp.getStudent();
+//        System.out.println(student);
+//
+//        // 직접적인 널 처리방법
+//        String studentName;
+//        if (student != null){
+//            studentName = student.getName();
+//        }
+//        else{
+//            studentName = "등록된 학생이 아닙니다";
+//        }
+
+        // 2. NullPointerException
+        // 이름이 아직 존재하지 않는데 호출하기에
+        // --> uncheckException이라서 빨간줄은 안뜬다 - 대신 프로그램이 비정상적 종료
+        // 그래서 직접 널 처리 해주기
+//        String studentName = student.getName();
+//        System.out.println(studentName);
+
+    }
 }
